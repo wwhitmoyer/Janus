3843a2944dc15d6e3182529aaae120f1879b2d5d
wwhitmoyer <wade.whitmoyer@gmail.com>
2018-03-15 10:48:49 -0400

    TCP Clinet Added

---
+++ Firmware/Janus/Janus.ino
@@ -0,0 +1,330 @@
+// Janus Project
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+
+ESP8266WebServer server(80);
+ 
+#define MAX_BITS 64                 // max number of bits 
+#define WEIGAND_WAIT_TIME  3000      // time to wait for another weigand pulse.  
+unsigned long startTime = millis(); 
+unsigned char databits[MAX_BITS];    // stores all of the data bits
+unsigned char bitCount;              // number of bits currently captured
+unsigned char flagDone;              // goes low when data is currently being captured
+unsigned int weigand_counter;        // countdown until we assume there are no more bits
+ 
+unsigned long facilityCode=0;        // decoded facility code
+unsigned long cardCode=0;            // decoded card code
+const char* ssid = "test";
+const char* passphrase = "test";
+String st;
+String content;
+int statusCode;
+const char* message;
+WiFiServer telnetServer(23);
+WiFiClient serverClient; 
+// interrupt that happens when INTO goes low (0 bit)
+void ISR_INT0()
+{
+  //Serial.print("0");   // uncomment this line to display raw binary
+  bitCount++;
+  flagDone = 0;
+  weigand_counter = WEIGAND_WAIT_TIME;  
+ 
+}
+ 
+// interrupt that happens when INT1 goes low (1 bit)
+void ISR_INT1()
+{
+  //Serial.print("1");   // uncomment this line to display raw binary
+  databits[bitCount] = 1;
+  bitCount++;
+  flagDone = 0;
+  weigand_counter = WEIGAND_WAIT_TIME;  
+}
+ 
+void setup()
+{
+  unsigned long startTime = millis();
+  pinMode(13, OUTPUT);  // LED
+  pinMode(D2, INPUT);     // DATA0 (INT0)
+  pinMode(D1, INPUT);     // DATA1 (INT1)
+ 
+  Serial.begin(9600);
+  Serial.println("RFID Readers");
+ 
+  // binds the ISR functions to the falling edge of INTO and INT1
+  attachInterrupt(D2, ISR_INT0, FALLING);  
+  attachInterrupt(D1, ISR_INT1, FALLING);
+  weigand_counter = WEIGAND_WAIT_TIME;
+    Serial.begin(115200);
+    WiFi.disconnect();
+  EEPROM.begin(512);
+  delay(10);
+  Serial.println();
+  Serial.println();
+  Serial.println("Startup");
+  // read eeprom for ssid and pass
+  Serial.println("Reading EEPROM ssid");
+  String esid;
+  for (int i = 0; i < 32; ++i)
+    {
+      esid += char(EEPROM.read(i));
+    }
+  Serial.print("SSID: ");
+  Serial.println(esid);
+  Serial.println("Reading EEPROM pass");
+  String epass = "";
+  for (int i = 32; i < 96; ++i)
+    {
+      epass += char(EEPROM.read(i));
+    }
+  Serial.print("PASS: ");
+  Serial.println(epass);  
+  if ( esid.length() > 1 ) {
+      WiFi.begin(esid.c_str(), epass.c_str());
+      if (testWifi()) {
+        launchWeb();
+        telnetServer.begin();
+  telnetServer.setNoDelay(true);
+  Serial.println("Please connect Telnet Client, exit with ^] and 'quit'");
+
+  Serial.print("Free Heap[B]: ");
+  Serial.println(ESP.getFreeHeap());
+        return;
+      } 
+  }
+  setupAP();
+}
+ 
+void loop()
+{
+    server.handleClient();
+  // This waits to make sure that there have been no more data pulses before processing data
+  if (!flagDone) {
+    if (--weigand_counter == 0)
+      flagDone = 1;  
+  }
+ if (telnetServer.hasClient()) {
+    if (!serverClient || !serverClient.connected()) {
+      if (serverClient) {
+        serverClient.stop();
+        Serial.println("Telnet Client Stop");
+      }
+      serverClient = telnetServer.available();
+      Serial.println("New Telnet client");
+      serverClient.flush();  // clear input buffer, else you get strange characters 
+    }
+  }
+
+    while(serverClient.available()) {  // get data from Client
+    //Serial.write(serverClient.read());
+    message = serverClient.read();
+    Serial.println(message);
+  }
+
+  if (message == "poll") { // run every 2000 ms
+      serverClient.print("I am Here");
+    message = "";
+}
+message = "";
+  delay(10);  // to avoid strange characters left in buffer
+  // if we have bits and we the weigand counter went out
+  if (bitCount > 0 && flagDone) {
+    unsigned char i;
+ 
+    Serial.print("Read ");
+    int x = 0;
+     while (x != bitCount) 
+      {
+         Serial.print(databits[x++]);
+      }
+    Serial.print(" ");
+    Serial.print(bitCount);
+    Serial.print(" bits. ");
+ 
+    // we will decode the bits differently depending on how many bits we have
+    // see www.pagemac.com/azure/data_formats.php for mor info
+    if (bitCount == 35)
+    {
+      // 35 bit HID Corporate 1000 format
+      // facility code = bits 2 to 14
+      for (i=2; i<14; i++)
+      {
+         facilityCode <<=1;
+         facilityCode |= databits[i];
+      }
+ 
+      // card code = bits 15 to 34
+      for (i=14; i<34; i++)
+      {
+         cardCode <<=1;
+         cardCode |= databits[i];
+      }
+ 
+      printBits();
+    }
+    else if (bitCount == 26)
+    {
+      // standard 26 bit format
+      // facility code = bits 2 to 9
+      for (i=1; i<9; i++)
+      {
+         facilityCode <<=1;
+         facilityCode |= databits[i];
+      }
+ 
+      // card code = bits 10 to 23
+      for (i=9; i<25; i++)
+      {
+         cardCode <<=1;
+         cardCode |= databits[i];
+      }
+ 
+      printBits();  
+    }
+    else {
+      // you can add other formats if you want!
+     Serial.println("Unable to decode."); 
+    }
+ 
+     // cleanup and get ready for the next card
+     bitCount = 0;
+     facilityCode = 0;
+     cardCode = 0;
+     for (i=0; i<MAX_BITS; i++) 
+     {
+       databits[i] = 0;
+     }
+  }
+}
+ 
+void printBits()
+{
+      // I really hope you can figure out what this function does
+      Serial.print("FC = ");
+      Serial.print(facilityCode);
+      Serial.print(", CC = ");
+      Serial.println(cardCode); 
+}
+bool testWifi(void) {
+  int c = 0;
+  Serial.println("Waiting for Wifi to connect");  
+  while ( c < 20 ) {
+    if (WiFi.status() == WL_CONNECTED) { return true; } 
+    delay(500);
+    Serial.print(WiFi.status());    
+    c++;
+  }
+  Serial.println("");
+  Serial.println("Connect timed out, opening AP");
+  return false;
+} 
+
+void launchWeb() {
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer();
+  // Start the server
+  server.begin();
+  Serial.println("Server started"); 
+}
+
+void setupAP(void) {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      st += "<li>";
+      st += WiFi.SSID(i);
+      st += " (";
+      st += WiFi.RSSI(i);
+      st += ")";
+      st += (WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*";
+      st += "</li>";
+    }
+  st += "</ol>";
+  delay(100);
+  WiFi.softAP(ssid, passphrase, 6);
+  Serial.println("softap");
+  launchWeb();
+  Serial.println("over");
+}
+}
+
+void createWebServer(){
+
+    server.on("/", []() {
+        IPAddress ip = WiFi.softAPIP();
+        String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+        content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
+        content += ipStr;
+        content += "<p>";
+        content += st;
+        content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+        content += "</html>";
+        server.send(200, "text/html", content);  
+    });
+    server.on("/setting", []() {
+        String qsid = server.arg("ssid");
+        String qpass = server.arg("pass");
+        if (qsid.length() > 0 && qpass.length() > 0) {
+          Serial.println("clearing eeprom");
+          for (int i = 0; i < 96; ++i) { EEPROM.write(i, 0); }
+          Serial.println(qsid);
+          Serial.println("");
+          Serial.println(qpass);
+          Serial.println("");
+            
+          Serial.println("writing eeprom ssid:");
+          for (int i = 0; i < qsid.length(); ++i)
+            {
+              EEPROM.write(i, qsid[i]);
+              Serial.print("Wrote: ");
+              Serial.println(qsid[i]); 
+            }
+          Serial.println("writing eeprom pass:"); 
+          for (int i = 0; i < qpass.length(); ++i)
+            {
+              EEPROM.write(32+i, qpass[i]);
+              Serial.print("Wrote: ");
+              Serial.println(qpass[i]); 
+            }    
+          EEPROM.commit();
+          content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+          statusCode = 200;
+        } else {
+          content = "{\"Error\":\"404 not found\"}";
+          statusCode = 404;
+          Serial.println("Sending 404");
+        }
+        server.send(statusCode, "application/json", content);
+        delay(2000);
+        ESP.restart();
+    });
+     server.on("/cleareeprom", []() {
+      content = "<!DOCTYPE HTML>\r\n<html>";
+      content += "<p>Clearing the EEPROM</p></html>";
+      server.send(200, "text/html", content);
+      Serial.println("clearing eeprom");
+      for (int i = 0; i < 96; ++i) { EEPROM.write(i, 0); }
+      EEPROM.commit();
+      delay(2000);
+        ESP.restart();
+    });
+}
